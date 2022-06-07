2f741ae122be3450f53291a8d861eacc05eac6e5
manikandan <vmmass08@gmail.com>
2022-06-07 15:00:04 +0530

    addfile.

---
+++ prime-with-range.js
@@ -0,0 +1,16 @@
+
+//  prime with  range
+
+let n = 15;
+for(var i=2; i<= n; i++){
+ a = 0;
+ for(var j = 2; j <= i/2 ; j ++){
+     if(i % j==0){
+       a = 1;  
+     }
+ }
+ if(a == 0){
+     console.log("prime number=" +i)
+ }
+ 
+}
